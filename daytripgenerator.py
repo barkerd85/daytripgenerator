86c12c8c542ee0ad9e9c01cef515e050e4c8212e
Dustin Barker <barkerd85@yahoo.com>
2021-10-07 17:51:57 -0600

    refactored_polished

--- daytripgenerator.py
+++ daytripgenerator.py
@@ -4,7 +4,7 @@ import random
 
 
 cities = ['Cincinnati', 'Chicago', 'Dallas']
-randcity = random.choice(cities) 
+
 
 
 restaraunts_in_Cincinnati = ('Skyline Chili', 'Frishces', 'Larosas')
@@ -66,19 +66,19 @@ def random_city():
 
     if randcity == 'Cincinnati': 
         transportation = set_transportation_cincy()
-        activity = random.choice (activities_in_Cincinnati)
-        food = random.choice (restaraunts_in_Cincinnati)
+        activity = set_activity_cincy()
+        food = set_food_cincy()
     
     if randcity == 'Dallas': 
-        transportation = random.choice (transportation_in_Dallas)
-        activity = random.choice (activities_in_Dallas)
-        food = random.choice (restaraunts_in_Dallas )
+        transportation = set_transportation_dallas()
+        activity = set_activity_dallas()
+        food = set_food_dallas()
 
 
     if randcity == 'Chicago': 
-        transportation = random.choice (transportation_in_Chicago)
-        activity = random.choice (activities_in_Chicago)
-        food = random.choice (restaraunts_in_Chicago )
+        transportation = set_transportation_chicago()
+        activity = set_activity_chicago()
+        food = set_food_chicago()
 
     return [randcity, transportation, activity, food]
 
@@ -87,52 +87,55 @@ print(f'Your day trip is planned to {activities_list[0]} where you will be ridin
 question = input ('Are you happy with your day trip selections?')
 
 while question != 'yes' and question != 'no':
-    question = input('please answer yes or no')
+    question = input('Please answer yes or no.')
 
-while question == ('no'):
-    print('well you are dumb because that looks like fun!')
-    question2 = input('What would you like to change?')
-    if question2 == 'city':
-        activities_list2 = random_city()
-        while activities_list[0] == activities_list2 [0]:
-            activities_list2 = random_city()
-        activities_list = activities_list2
-        print(f'Your day trip is planned to {activities_list[0]} where you will be riding in style with {activities_list[1] }, and having dinner at {activities_list[3]}. Then you will be enjoying the {activities_list[2]}. ')
+while question != ('yes'):
+    if question != ('no'):
+        question = input('Please answer yes or no.')
+    else:    
         
-        question = input ('Are you happy with your day trip selections?')       
-    elif question2 == 'transportation':
-        if activities_list[0] == 'Chicago':
-            activities_list[1] = set_transportation_chicago()
-        if activities_list[0] == 'Dallas':
-            activities_list[1] = set_transportation_dallas()
-        if activities_list[0] == 'Cincinnati':
-            activities_list[1] = set_transportation_cincy()
-        print(f'Your day trip is planned to {activities_list[0]} where you will be riding in style with {activities_list[1] }, and having dinner at {activities_list[3]}. Then you will be enjoying the {activities_list[2]}. ')
-        question = input ('Are you happy with your day trip selections?')
+        question2 = input('What would you like to change?')
+        if question2 == 'city':
+            activities_list2 = random_city()
+            while activities_list[0] == activities_list2 [0]:
+                activities_list2 = random_city()
+            activities_list = activities_list2
+            print(f'Your day trip is planned to {activities_list[0]} where you will be riding in style with {activities_list[1] }, and having dinner at {activities_list[3]}. Then you will be enjoying the {activities_list[2]}. ')
+            
+            question = input ('Are you happy with your day trip selections?')       
+        elif question2 == 'transportation':
+            if activities_list[0] == 'Chicago':
+                activities_list[1] = set_transportation_chicago()
+            if activities_list[0] == 'Dallas':
+                activities_list[1] = set_transportation_dallas()
+            if activities_list[0] == 'Cincinnati':
+                activities_list[1] = set_transportation_cincy()
+            print(f'Your day trip is planned to {activities_list[0]} where you will be riding in style with {activities_list[1] }, and having dinner at {activities_list[3]}. Then you will be enjoying the {activities_list[2]}. ')
+            question = input ('Are you happy with your day trip selections?')
 
 
-    elif question2 == 'food':
-        if activities_list[0] == 'Chicago':
-            activities_list[3] = set_food_chicago()
-        if activities_list[0] == 'Dallas':
-            activities_list[3] = set_food_dallas()
-        if activities_list[0] == 'Cincinnati':
-            activities_list[3] = set_food_cincy()
-        print(f'Your day trip is planned to {activities_list[0]} where you will be riding in style with {activities_list[1] }, and having dinner at {activities_list[3]}. Then you will be enjoying the {activities_list[2]}. ')
-        question = input ('Are you happy with your day trip selections?')
+        elif question2 == 'food':
+            if activities_list[0] == 'Chicago':
+                activities_list[3] = set_food_chicago()
+            if activities_list[0] == 'Dallas':
+                activities_list[3] = set_food_dallas()
+            if activities_list[0] == 'Cincinnati':
+                activities_list[3] = set_food_cincy()
+            print(f'Your day trip is planned to {activities_list[0]} where you will be riding in style with {activities_list[1] }, and having dinner at {activities_list[3]}. Then you will be enjoying the {activities_list[2]}. ')
+            question = input ('Are you happy with your day trip selections?')
 
 
-    elif question2 == 'activity':
-        if activities_list[0] == 'Chicago':
-            activities_list[2] = set_activity_chicago()
-        if activities_list[0] == 'Dallas':
-            activities_list[2] = set_activity_dallas()
-        if activities_list[0] == 'Cincinnati':
-            activities_list[2] = set_activity_cincy()
-        print(f'Your day trip is planned to {activities_list[0]} where you will be riding in style with {activities_list[1] }, and having dinner at {activities_list[3]}. Then you will be enjoying the {activities_list[2]}. ')
-        question = input ('Are you happy with your day trip selections?')
-    else:
-        print('please pick city, transportation, food or activity')
+        elif question2 == 'activity':
+            if activities_list[0] == 'Chicago':
+                activities_list[2] = set_activity_chicago()
+            if activities_list[0] == 'Dallas':
+                activities_list[2] = set_activity_dallas()
+            if activities_list[0] == 'Cincinnati':
+                activities_list[2] = set_activity_cincy()
+            print(f'Your day trip is planned to {activities_list[0]} where you will be riding in style with {activities_list[1] }, and having dinner at {activities_list[3]}. Then you will be enjoying the {activities_list[2]}. ')
+            question = input ('Are you happy with your day trip selections?')
+        else:
+            print('Please pick city, transportation, food, or activity.')
 
 print(f'Your day trip is planned to {activities_list[0]} where you will be riding in style with {activities_list[1] }, and having dinner at {activities_list[3]}. Then you will be enjoying the {activities_list[2]}. ')
 print('enjoy yourself')
